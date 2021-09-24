9f8c5c3d5885b27fefab9a91732b81799b3e5cb2
bharat <bharatchoithani10@gmail.com>
2021-09-24 16:30:54 +0530

    added exp5.java

---
+++ exp5.java
@@ -0,0 +1,11 @@
+public class exp5
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println("HELLO");
+    }
+}
